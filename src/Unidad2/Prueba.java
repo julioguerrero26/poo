72752a45d03cd9995c9b4c6c54e50b427d0d37bb
Juju <Juliocesarng25@gmail.com>
2025-04-28 02:20:16 -0600

    creado por juju

--- src/Unidad2/Prueba.java
+++ src/Unidad2/Prueba.java
@@ -1,53 +1,24 @@
 package Unidad2;
 
-import javax.swing.*;
 
 public class Prueba{
     public static void main(String[] args) {
-        try {
-            double A = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite el primer número:"));
-            double B = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite el segundo número:"));
-            double C = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite el tercer número:"));
-
-            double mayor, medio, menor;
-
-            if (A >= B && A >= C) {
-                mayor = A;
-                if (B >= C) {
-                    medio = B;
-                    menor = C;
-                } else {
-                    medio = C;
-                    menor = B;
-                }
-            } else if (B >= A && B >= C) {
-                mayor = B;
-                if (A >= C) {
-                    medio = A;
-                    menor = C;
-                } else {
-                    medio = C;
-                    menor = A;
-                }
-            } else {
-                mayor = C;
-                if (A >= B) {
-                    medio = A;
-                    menor = B;
-                } else {
-                    medio = B;
-                    menor = A;
-                }
+        System.out.println("                           ________________");
+        System.out.println("                        ,===:'.,            `-._");
+        System.out.println("                             :.---.__         `-._");
+        System.out.println("                               `:.     `--.         `.");
+        System.out.println("                                 \\.        `.         `.");
+        System.out.println("                         (,,(,    \\.         .   ____,-.,");
+        System.out.println("                      (,'     /   \\.   ,--.___.'");
+        System.out.println("                  ,  ,'  ,--.  `,   \\.;'         `");
+        System.out.println("                   `{D, {    \\  :    \\;");
+        System.out.println("                     V,,'    /  /    //");
+        System.out.println("                     j;;    /  ,' ,-//.    ,---.      ,");
+        System.out.println("                     \\;'   /  ,' /  _  \\  /  _  \\   ,'/");
+        System.out.println("                           \\   `'  / \\  `'  / \\  `.' /");
+        System.out.println("                            `.,'   `.,'   `._,'");
+        System.out.println("                                |             |");
+        System.out.println("                                 `._         _,'");
+        System.out.println("                                    `-------'");
             }
-
-            String message = String.format("Números Ordenados\n" +
-                    "Mayor = "+mayor+"\n"+
-                    "Medio = "+medio+"\n"+
-                    "Menor = "+menor);
-            JOptionPane.showMessageDialog(null, message);
-
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "Error: Por favor ingrese números válidos.");
-        }
-    }
 }
